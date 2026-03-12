315e0e4947fb332dc2a228cd595e1dd4c0c79205
Jose Sandoval <jsandovalz@gmail.com>
2026-03-12 11:52:06 -0400

    Add File type in slides utils

--- iglesiaepseranzaenel/utils/slides.ts
+++ iglesiaepseranzaenel/utils/slides.ts
@@ -1,9 +1,16 @@
+type File = {
+  id: number;
+  documentId: string;
+  name: string;
+  url: string;
+}
+
 type Imagen = {
   alt: string;
   caption: string;
   button_text: string;
   button_link: string;
-  src?: { data?: { attributes: { url: string } }[] };
+  src?: File[];
 };
 
 type PaginaData = {
@@ -12,16 +19,16 @@ type PaginaData = {
 };
 
 export function getSlidesByPage(data: PaginaData[], pageName: string) {
-  const slideInicio = data.find(item => item.Pagina ===pageName);
+  const slideInicio = data.find((item: PaginaData) => item.Pagina ===pageName);
   
   return slideInicio?.Imagenes.flatMap(img => {
-  const files = img?.src ?? [];
+  const files: File[] = img?.src ?? [];
   return files.map(file => ({
-    src: process.env.API_URL + file.url, // aquí concatenas tu dominio
+    src: process.env.API_URL + file.url, 
     alt: img.alt,
     caption: img.caption,
     buttonText: img.button_text,
     buttonLink: img.button_link,
   }));
-})
+}) ?? []
 };
