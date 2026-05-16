34480bd317041a61cf09a06d425b12dfd9c88e85
Jose Sandoval <jsandovalz@gmail.com>
2026-05-15 22:25:36 -0400

    Instance oh healthy

--- backend-iglesia/src/api/actividades_semana/content-types/actividades_semana/lifecycles.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  async afterCreate() {
-    await strapi.reload();
-  },
-  async afterUpdate() {
-    await strapi.reload();
-  }
-};
--- backend-iglesia/src/api/actividades_semana/controllers/actividades_semana.ts
+++ backend-iglesia/src/api/actividades_semana/controllers/actividades_semana.ts
@@ -1,4 +1,3 @@
 import { factories } from '@strapi/strapi';
 
 export default factories.createCoreController('api::actividades_semana.actividades_semana');
-
