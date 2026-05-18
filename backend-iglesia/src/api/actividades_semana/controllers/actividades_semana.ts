8a410239ae623a21ac4c51cdc5972bdaee4ea2f9
Jose Sandoval <jsandovalz@gmail.com>
2026-05-18 10:35:05 -0400

    refactor sigular name

--- backend-iglesia/src/api/actividades_semana/controllers/actividades_semana.ts
+++
@@ -1,3 +0,0 @@
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreController('api::actividades_semana.actividades_semana');
--- backend-iglesia/src/api/actividades_semana/routes/actividades_semana.ts
+++
@@ -1,3 +0,0 @@
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreRouter('api::actividades_semana.actividades_semana');
