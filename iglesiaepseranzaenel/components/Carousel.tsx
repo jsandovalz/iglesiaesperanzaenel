18d38179f3ad875acdb41a78d45c7181ac2445ea
Jose Sandoval <jsandovalz@gmail.com>
2026-06-01 11:45:37 -0400

    fix console error

--- iglesiaepseranzaenel/components/Carousel.tsx
+++ iglesiaepseranzaenel/components/Carousel.tsx
@@ -9,27 +9,25 @@ export default function Carousel({ items }: { items: any[] }) {
   const next = () => setCurrent((c) => (c + 1) % items.length);
   const prev = () => setCurrent((c) => (c - 1 + items.length) % items.length);
   const url = getURL();
+  items.map((item,i)=>console.log("URLIMAGe=",url+item.url));
 
   return (
     <div className="relative w-full h-[350px] overflow-hidden rounded-xl shadow-lg">
-      {items.map((item, i) => {
-        console.log("URLIMAGE=",url);
-        (
-          <div
-            key={i}
-            className={`absolute inset-0 transition-opacity duration-700 ${
-              i === current ? "opacity-100" : "opacity-0"
-            }`}
-          >
-            <Image
-              src={url+item.url}
-              alt={item.alternativeText || ""}
-              fill
-              className="object-cover"
-            />
-          </div>
-        )
-      })}
+      {items.map((item, i) => (
+        <div
+          key={i}
+          className={`absolute inset-0 transition-opacity duration-700 ${
+            i === current ? "opacity-100" : "opacity-0"
+          }`}
+        >
+          <Image
+            src={url+item.url}
+            alt={item.alternativeText || ""}
+            fill
+            className="object-cover"
+          />
+        </div>
+      ))}
 
       <button
         onClick={prev}
